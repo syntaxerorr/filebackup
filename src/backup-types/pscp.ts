3687cf7e2ba46fa0f341686489f6d6de05cce9de
Scott Ferguson <scott@toledocpu.com>
2020-02-15 23:49:43 -0500

    Better text for log

--- src/backup-types/pscp.ts
+++ src/backup-types/pscp.ts
@@ -5,7 +5,7 @@ import { getBackupTypeSpec, BackupType } from '../enums';
 * Putty pscp.exe
 */
 export async function pscp(source: string, destination: string) {
-  console.log(`Using: ${getBackupTypeSpec(BackupType.pscp).description}, ${source}, ${destination}`);
-  let test = await shellExe('ping 8.8.8.8');
-  console.log(`test: ${test}`);
+  console.log(`Running: ${getBackupTypeSpec(BackupType.pscp).description}, ${source} -> ${destination}`);
+  const output = await shellExe('ping 8.8.8.8');
+  console.log(`Output: ${output}`);
 }
--- src/backup-types/win_copy.ts
+++ src/backup-types/win_copy.ts
@@ -6,7 +6,7 @@ import { getBackupTypeSpec, BackupType } from '../enums';
 */
 const cmd = 'copy';
 export async function win_copy(source: string, destination: string) {
-  console.log(`Using: ${getBackupTypeSpec(BackupType.win_copy).description}, ${source} -> ${destination}`);
+  console.log(`Running: ${getBackupTypeSpec(BackupType.win_copy).description}, ${source} -> ${destination}`);
   let output = await shellExe(`${cmd} ${source} ${destination}`);
-  console.log(`output: ${output}`);
+  console.log(`Output: ${output}`);
 }
